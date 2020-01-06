ede6eb23a89df43054edf2a92d6b7ad8d27ed0b7
Amy Parent <amy@amyparent.com>
2020-01-06 15:54:44 +0000

    Fix some includes and such

--- include/compass/type/primitives.hpp
+++ include/compass/type/primitives.hpp
@@ -16,26 +16,32 @@
 #include <compass/type/expected.hpp>
 
 namespace Compass {
-    
+
     using namespace tl;
 
     template <typename T>
     using result = expected<T, std::string>;
-    
+
     using Int8 = std::int8_t;
     using UInt8 = std::uint8_t;
-    
+
     using Int16 = std::int16_t;
     using UInt16 = std::uint16_t;
-    
+
+    using Int32 = std::int32_t;
+    using UInt32 = std::uint32_t;
+
+    using Int64 = std::int64_t;
+    using UInt64 = std::uint64_t;
+
     using UInt = std::uint32_t;
     using Int = std::int32_t;
     using Float = float;
     using String = std::string;
-    
+
     template <typename T>
     using Array = std::vector<T>;
-    
+
     template <typename K, typename V>
     using Map = std::unordered_map<K, V>;
 }
